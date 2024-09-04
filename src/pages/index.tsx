7a1987b3fabd148ad0ee51d5f5ad05d13f928e8c
Ben Merken <ben.merken@appfoundry.be>
2024-09-04 16:29:21 +0200

    Add react-router-dom, and scaffold resume components

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+const HomePage = () => {
+  return (
+    <main>
+      <h1>Ben Merken</h1>
+      <p>Coming soon...</p>
+    </main>
+  )
+}
+
+export default HomePage
---
+++ src/pages/resume/index.tsx
@@ -0,0 +1,5 @@
+const ResumePage = () => {
+  return <main></main>
+}
+
+export default ResumePage
