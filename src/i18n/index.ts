75ceba55ea23a1fbe8f627cfa7add96193af3ef1
Ben Merken <ben.merken@gmail.com>
2024-09-11 21:05:48 +0200

    Set up i18next

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,10 +1,18 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-import resources from '@/i18n/resources';
+import portfolioEN from '@/i18n/locales/en-GB/portfolio.json';
+import portfolioNL from '@/i18n/locales/nl-BE/portfolio.json';
 
 i18n.use(initReactI18next).init({
-  resources,
+  resources: {
+    'en-GB': {
+      portfolio: portfolioEN
+    },
+    'nl-BE': {
+      portfolio: portfolioNL
+    }
+  },
   lng: 'en-GB',
   interpolation: {
     escapeValue: false
--- src/i18n/resources.ts
+++ src/i18n/resources.ts
@@ -1,13 +1,7 @@
 import portfolioEN from '@/i18n/locales/en-GB/portfolio.json';
-import portfolioNL from '@/i18n/locales/nl-BE/portfolio.json';
 
 const resources = {
-  'en-GB': {
-    portfolio: portfolioEN
-  },
-  'nl-BE': {
-    portfolio: portfolioNL
-  }
-};
+  portfolio: portfolioEN
+} as const;
 
 export default resources;
