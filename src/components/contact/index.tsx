09cdda5af3dc6233cf75e4754f8abd5fcf829b0c
Ben Merken <ben.merken@gmail.com>
2025-04-04 01:07:03 +0200

    Add skeleton for Contact component

---
+++ src/components/contact/index.tsx
@@ -0,0 +1,13 @@
+import './styles.scss';
+
+function Contact() {
+  return (
+    <section id="contact" className="contact">
+      <h2>Get in touch!</h2>
+      <p>Contrary to popular belief, I am rather approachable!</p>
+      {/* TODO: See if I can use a form instead of a mailto link for contact. Otherwise, better to link to LinkedIn, I guess. */}
+    </section>
+  );
+}
+
+export default Contact;
